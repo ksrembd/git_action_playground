b0e15cde7356176fb78cddc6a25d9b08fc10c870
kavuluri sriram <sriram.embeddedsolutions@gmail.com>
2025-11-14 13:12:49 +0100

    internal testing

--- platform/aas/test/fusa/test_fusa.py
+++ platform/aas/test/fusa/test_fusa.py
@@ -1,4 +1,4 @@
 print("hello from test_fusa.py")
 print("change from test_fusa.py")
-print("from new folder struct test_fusa.py")
-print("from new folder struct 2 test_fusa.py")
+print("change from internal test.py")
+
